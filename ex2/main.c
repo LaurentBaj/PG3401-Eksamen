feccbc35cf786fefa5daee913a4255109f5a7265
Laurent <laurent_baj@hotmail.com>
2021-11-04 15:17:28 +0100

    Progress inn text

--- ex2/main.c
+++ ex2/main.c
@@ -12,14 +12,12 @@ int main()
 		file2 = fopen ("decoded_text.txt", "w");
 		while (!feof(file1))
 		{
-			// As long as there is content (true) - put that into num
 			if (fscanf(file1, "%c", &c) == 1)
 			{
-				fwrite (&c, sizeof(char), 1, file2); // write num into bin
+				fwrite (&c, sizeof(char), 1, file2); 
 			}
 		}
 		fclose (file2); 
 	}	
 	fclose (file1); 
-	
 }
