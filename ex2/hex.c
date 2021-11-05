dc47a13665ae724a689976da988652362f7c56bf
Laurent <laurent_baj@hotmail.com>
2021-11-05 12:45:16 +0100

    Code format in 2a

--- ex2/hex.c
+++ ex2/hex.c
@@ -1,55 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-int getDec(int x)
+// Function that converts a hex to a decimal
+// Function is adapted from: https://codescracker.com/c/program/c-program-convert-hexadecimal-to-decimal.htm
+int toDecimal(char x)
 {
-    int val = 0;
-    if(x>='0' && x<='9')
-    {
-        val = x - 48;
-    }
-    else if(x>='a' && x<='f')
-    {
-        val = x - 97 + 10;
-    }
-    else if(x>='A' && x<='F')
-    {
-        val = x - 65 + 10;
-    }
-    return val; 
+    if(x>='0' && x<='9') return x-'0';
+    else if(x>='A' && x<='Z') return x-'A'+10;
+    else if(x>='a' && x<='z') return x-'a'+10;
 }
 
 int main()
 {
     FILE *encoded_file, *decoded_file; 
-    char c[2], buffer; 
+    char c1, c2, buffer; 
+    int left, right, result;
 
-    // Open hex.txt for reading - decoded_file.txt for writing
+    // Open hex.txt for reading, decoded_file.txt for writing
     encoded_file = fopen("hex.txt", "r");
     decoded_file = fopen("decoded_file.txt", "w"); 
 
+     
+    //  1. Go through hex.txt
+    //  2. For each iteration: 
+    //    - Convert each hex pair to a decimal
+    //    - Convert decimal to char + write to file 
     if (encoded_file != NULL)
     {
-        /*
-            Go through hex.txt
-            For each iteration: 
-                - Convert each hex pair to a decimal
-                - Convert decimal to char + write to file 
-        */
         while (!feof(encoded_file))
-        {        
+        {   
+            // Get pair of hex from hex.txt     
+            c1 = fgetc(encoded_file); // left
+            c2 = fgetc(encoded_file); // right
+            left = toDecimal(c1) * 16; // Account for base of left digit
+            right = toDecimal(c2);
             
-            c[0] = fgetc(encoded_file);
-            c[1] = fgetc(encoded_file);
-            int left = getDec(c[0]) * 16; 
-            int right = getDec(c[1]);
-
-            buffer = left + right; 
-            //printf("%c", buffer); 
-		    fwrite (&buffer, sizeof(char), 1, decoded_file);
+            result = left + right; 
+		    fprintf(decoded_file, "%c", result);
         }
     }
+    
+    // Close both streams 
     fclose(encoded_file); 
     fclose(decoded_file); 
+    return 0; 
 }
